6614f6d1febb3c8583b163a6128cfbb2a1bd30da
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-09-15 09:51:23 +0900

    add confirmation dialog

--- scripts/reset_presets_button.gd
+++ scripts/reset_presets_button.gd
@@ -4,9 +4,22 @@ extends Button
 func _ready() -> void:
     pressed.connect(
         func():
-            var dir := DirAccess.open("user://")
-            for filename in dir.get_files():
-                if filename.begins_with("preset_") and filename.ends_with(".json"):
-                    print("file deleted: %s" % filename)
-                    dir.remove(filename)
+            var dialog := ConfirmationDialog.new()
+            add_child(dialog)
+            dialog.get_ok_button().custom_minimum_size = Vector2(200, 100)
+            dialog.get_cancel_button().custom_minimum_size = Vector2(200, 100)
+            dialog.get_label().custom_minimum_size = Vector2(0, 100)
+            dialog.get_label().vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+            dialog.dialog_text = "Are you sure to remove preset files"
+            dialog.confirmed.connect(
+                _remove_preset_files
+            )
+            dialog.popup_centered()
     )
+
+static func _remove_preset_files() -> void:
+    var dir := DirAccess.open("user://")
+    for filename in dir.get_files():
+        if filename.begins_with("preset_") and filename.ends_with(".json"):
+            print("file deleted: %s" % filename)
+            dir.remove(filename)
