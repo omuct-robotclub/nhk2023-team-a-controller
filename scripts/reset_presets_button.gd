1f33bee7883419a78aed3a4c46c56b8a2e04e35c
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-09-15 10:23:24 +0900

    tweak confirmation dialog

--- scripts/reset_presets_button.gd
+++ scripts/reset_presets_button.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
             dialog.get_cancel_button().custom_minimum_size = Vector2(200, 100)
             dialog.get_label().custom_minimum_size = Vector2(0, 100)
             dialog.get_label().vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-            dialog.dialog_text = "Are you sure to remove preset files"
+            dialog.dialog_text = "Are you sure to you want to delete saved presets?"
             dialog.confirmed.connect(
                 _remove_preset_files
             )
