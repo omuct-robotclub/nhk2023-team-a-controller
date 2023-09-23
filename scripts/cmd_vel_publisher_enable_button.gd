1d520445e182ca310105f61bed721986e10dafb0
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-09-23 11:57:57 +0900

    change background color when activated

--- scripts/cmd_vel_publisher_enable_button.gd
+++ scripts/cmd_vel_publisher_enable_button.gd
@@ -1,15 +1,19 @@
 extends CheckButton
 
+@export var activated_color := Color.AQUAMARINE
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     set_pressed_no_signal(RobotInterface.cmd_vel_publisher_enabled)
+    RenderingServer.set_default_clear_color(activated_color)
     toggled.connect(
         func(_button_pressed: bool) -> void:
             RobotInterface.set_cmd_vel_publisher_enabled(button_pressed)
+            if button_pressed:
+                RenderingServer.set_default_clear_color(activated_color)
+            else:
+                RenderingServer.set_default_clear_color(Color(0.3, 0.3, 0.3, 1.0))
     )
     RobotInterface.cmd_vel_publisher_enabled_changed.connect(
         func() -> void:
             button_pressed = RobotInterface.cmd_vel_publisher_enabled
     )
-
