a0e2c6200fefcc74a928ea7da4dad87ed524d1e6
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-10-29 16:21:09 +0900

    change expander length

--- scripts/robot_controller.gd
+++ scripts/robot_controller.gd
@@ -185,7 +185,7 @@ func _expand_all() -> void:
     _working = true
     RobotInterface.set_donfan_cmd(1)
     await get_tree().create_timer(1.0).timeout
-    RobotInterface.set_expander_length(1.0)
+    RobotInterface.set_expander_length(0.9)
     RobotInterface.set_arm_angle(deg_to_rad(90))
     await get_tree().create_timer(1.0).timeout
     RobotInterface.set_arm_length(0.0)
