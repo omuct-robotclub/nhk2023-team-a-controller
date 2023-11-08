4c3fbceed104cec0c22c0b6653c56423ac57a873
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-11-08 12:55:49 +0900

    fix a stupid bug

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,6 @@ extends Node
 const WebsocketTransport = preload("res://scripts/websocket_transport.gd")
 
 func _ready() -> void:
-    pass
-#    var tp = WebsocketTransport.new()
-#    tp.open()
-#    rosbridge.add_transport(tp)
+    var tp = WebsocketTransport.new()
+    tp.open()
+    rosbridge.add_transport(tp)
